4f5151999cf0f187e98d0c36a1ef4007fc4cb165
Oshani <oshanirangi99@gmail.com>
2026-05-08 09:15:37 +0530

    feat: wrap application with ToastProvider in entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { ToastProvider } from './context/ToastContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* ToastProvider wraps the whole app so any component can call useToast() */}
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>,
 )
